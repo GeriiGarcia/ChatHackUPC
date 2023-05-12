ffade48046e5f17fbb48a7e564f875a8a9542569
GeriiGarcia <gerardgarciagros@gmail.com>
2023-05-12 22:28:14 +0200

    new branch here

--- servidor.c
+++ servidor.c
@@ -11,13 +11,33 @@
 #include <poll.h>
 
 
+/*  Basicametn ha d'enviar d'un client a l'altre els missatges
+    Els client li enviaran la ip a la que volen conectar-se al servidor i el servidor gestionará.
+
+    Array dinàmic que guardi Clients (nom, ip, (contra))
+    Si un usuari es conecta amb una altre ip, la ip del usuari canvia.
+
+    El servidor ha de interpretar comandes que li passi el client amb el format:    ::function
+
+
+    Serveis del servidor als clients:
+        - Mostrar menú usuaris disponibles
+        - conectar a un usuari
+        - Enviar missatges entre usuaris
+        - Acabar conexió amb usuari
+        - Desconectarse del servidor
+        - 
+    
+    Funcionament del Servidor*/
+
+
 #define SERVER_PORT 8081
 
 
 int main(int argc, char **argv)
 {
 
-    
+
 
 
 
